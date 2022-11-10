d69d80e87469db29cf5f9036bc4c22a5f98538ae
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2022-11-10 15:04:21 +0100

    Initial Commit

---
+++ src/main/java/de/neuefische/muc222booklibrary/Muc222BookLibraryApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.muc222booklibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Muc222BookLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Muc222BookLibraryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/neuefische/muc222booklibrary/Muc222BookLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.muc222booklibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Muc222BookLibraryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
